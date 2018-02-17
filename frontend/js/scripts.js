27e71396a20fda678cd01a5eb29fa9bdaf3c418c
Yeonwoo Lee <ylee100@u.rochester.edu>
2018-02-17 11:12:53 -0500

    Pic Loads Well

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -12,7 +12,7 @@ function resizeGame(){
 //Create a PIXI Application
 let app = new PIXI.Application({
   backgroundColor: 0xffcc00,
-  antialias: true,
+  antialias: false,
   }
 );
 app.renderer.view.style.position = "absolute";
@@ -22,26 +22,39 @@ window.onload = resizeGame;
 
 document.body.appendChild(app.view);
 
-//let texture = PIXI.utils.TextureCache["../res/rank1.png"];
-//let sprite = new PIXI.Sprite(texture);
 
+// Put Image
 function setup(){
   // load fininsh for image
   let rank1 = new PIXI.Sprite(
-    PIXI.loader.resources["../res/rank1.png"].texture
+    PIXI.loader.resources["res/rank1.png"].texture
   );
+  let rank7 = new PIXI.Sprite(
+    PIXI.loader.resources["res/rank7.png"].texture
+  );
+  rank1.scale = new PIXI.Point(0.15, 0.15);
+  rank7.scale = new PIXI.Point(0.15, 0.15);
 
+  rank7.x = 50;
+  rank7.y = 50;
   app.stage.addChild(rank1);
+  app.stage.addChild(rank7);
 }
 
-PIXI.loader
-  .add([
-    "../res/rank1.png",
-    "../res/rank2.png",
-    "../res/rank3.png",
-    "../res/rank4.png",
-    "../res/rank5.png",
-    "../res/rank6.png",
-    "../res/rank7.png",
-  ])
-  .load(setup);
+function loadSprite(){
+  PIXI.loader
+    .add([
+      "res/rank1.png",
+      "res/rank2.png",
+      "res/rank3.png",
+      "res/rank4.png",
+      "res/rank5.png",
+      "res/rank6.png",
+      "res/rank7.png",
+    ])
+    .load(setup);
+}
+
+
+//test
+loadSprite();
