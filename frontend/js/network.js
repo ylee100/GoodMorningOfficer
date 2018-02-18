3d968e6c72a6b19d885c0984451bfd766cc7abf0
Donghyeon Kim <donghyeon@tacocube.com>
2018-02-18 02:14:26 -0500

    User HTTPS for Socket.io

--- frontend/js/network.js
+++ frontend/js/network.js
@@ -1,4 +1,4 @@
-var socket = io('http://good-morning-officer.herokuapp.com');
+var socket = io('https://good-morning-officer.herokuapp.com');
 
 // Echo log from socket.io server.
 socket.on('log', (data) => console.log(data));
