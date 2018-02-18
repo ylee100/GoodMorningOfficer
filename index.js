021e845c2ee55821699432156752ff11323d1105
Donghyeon Kim <donghyeon@tacocube.com>
2018-02-17 21:26:24 -0500

    Replace Tab Characters with Spaces

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
-	path: '/socket',
-	pingTimeout: 3000
+  path: '/socket',
+  pingTimeout: 3000
 });
 
 app.use(express.static('frontend'));
